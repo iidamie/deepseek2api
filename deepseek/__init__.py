c0a4b9c9fee991369f3b98f40ff8140c3d4d5d33
iidamie <2279897399@qq.com>
2026-03-22 02:30:26 +0800

    fix: 更新 deepseek 模块导出，移除已删除的函数

--- deepseek/__init__.py
+++ deepseek/__init__.py
@@ -1,10 +1,10 @@
-from .pow import solve_pow_challenge, get_pow_response
+from .pow import get_pow_response, compute_pow_answer
 from .session import create_session, delete_deepseek_session
 from .api import call_deepseek_completion, stop_deepseek_stream
 
 __all__ = [
-    "solve_pow_challenge",
     "get_pow_response",
+    "compute_pow_answer",
     "create_session",
     "delete_deepseek_session",
     "call_deepseek_completion",
